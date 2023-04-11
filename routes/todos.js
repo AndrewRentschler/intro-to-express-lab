aaf81d928f0c7dbe0e404e38edf67ef103e2efe8
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-04-11 11:01:48 -0500

    add todos ctrl

--- routes/todos.js
+++ routes/todos.js
@@ -1,16 +1,12 @@
 import { Router } from 'express'
-import { todos } from '../data/todo-data.js'
+import * as todosCtrl from "../controllers/todos.js"
 
 const router = Router()
 
 // ALL ROUTES IN THIS FILE EXIST ON localhost:3000/todos
 
 // GET localhost:3000/todos
-router.get('/', function(req, res) {
-  res.render('todos/index', {
-    todos: todos
-  })
-})
+router.get('/', todosCtrl.index)
 
 // GET localhost:3000/todos/shrek
 router.get('/shrek', function(req, res) {
